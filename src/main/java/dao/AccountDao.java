b69ac5ba6c79173ef60f3deaeaca7c4f8be87b30
rereremi <20214105@kcska.onmicrosoft.com>
2023-10-31 14:56:53 +0900

    ログイン画面製作途中

--- src/main/java/dao/AccountDao.java
+++ src/main/java/dao/AccountDao.java
@@ -1,5 +1,33 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import model.UserModel;
+
 public class AccountDao extends DaoBase {
 	
+	public UserModel login(String userId,String password) {
+		 UserModel usermodel = null;
+	
+		try {
+			
+			String sql = "SELECT ID,PASSWORD";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, getUserId());
+			pStmt.setString(2, getPassWord());
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			
+			if (rs.next()) {
+				String userId = rs.getString("USER_ID");
+				String userId = rs.getString("USER_ID");
+				
+			}
+			
+		}
+	}
 }
---
+++ src/main/java/servlet/account/LoginServlet.java
@@ -0,0 +1,35 @@
+package servlet.account;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+    	dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		String userId  = request.getParameter("userId");
+		String password = request.getParameter("password"); 
+	}
+}
