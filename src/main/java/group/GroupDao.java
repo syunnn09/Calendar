b45909be253b7fa75c8feaf8a1099337d3d9b210
KCSNET\st20234162 <20234162@kcska.onmicrosoft.com>
2023-11-28 14:33:12 +0900

    Revert "Revert "[add] ユーザ一覧機能""
    This reverts commit 7ae02f13fd3432836fef3ed2a62ee6d0b0b709eb.

--- src/main/java/group/GroupDao.java
+++ src/main/java/group/GroupDao.java
@@ -1,22 +1,23 @@
 package group;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dao.DaoBase;
 
 public class GroupDao extends DaoBase {
-	private Connection conn;
 	private PreparedStatement pStmt;
 	
-	public void insert(int userId,int roomId) {
+	public void insert(int[] userIds,int roomId) {
 		try {
 			open();
-			String sql="Insert Into Group(userId,roomId)values(?,?)";
-			pStmt=conn.prepareStatement(sql);
-			pStmt.setInt(1, userId);
-			pStmt.setInt(2, roomId);
-			pStmt.executeUpdate();
+			for(int i=0;i<userIds.length;i++) {				
+				String sql="Insert Into joins(userId, roomId) values(?, ?)";
+				pStmt=conn.prepareStatement(sql);
+				pStmt.setInt(1, userIds[i]);
+				pStmt.setInt(2, roomId);
+				pStmt.executeUpdate();
+			}
 			close();
 		
 		}catch(SQLException e) {
@@ -25,18 +26,52 @@ public class GroupDao extends DaoBase {
 		
 	}
 	
-	public void grant(int roomId,int userId) {
-		try {			
+	public void grant(int[] userIds,int roomId) {
+		try {
 			open();
-			String sql="UPDATE group SET isAdmin = 1 WHERE userId = ? AND roomId = ? ;";
+			String sql ="UPDATE joins SET isAdmin = 0";
 			pStmt=conn.prepareStatement(sql);
-			pStmt.setInt(1, userId);
-			pStmt.setInt(2, roomId);
-			pStmt.executeUpdate();
+			pStmt.executeUpdate();			
+			for(int i=0;i<userIds.length;i++) {				
+				sql="UPDATE joins SET isAdmin = 1 WHERE userId = ? AND roomId = ? ;";
+				pStmt=conn.prepareStatement(sql);
+				pStmt.setInt(1, userIds[i]);
+				pStmt.setInt(2, roomId);
+				pStmt.executeUpdate();
+			}
 			close();
 			
 		}catch(SQLException e) {
 			
 		}
+	}
+	
+	public ResultSet adminselect(int roomId) {
+		try {			
+			open();
+			String sql = "SELECT users.userId, name, email, isAdmin FROM users,joins WHERE users.userId=joins.userId AND roomId=?;";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setInt(1, roomId);
+			ResultSet rs = pStmt.executeQuery(sql);
+			close();
+			return rs;
+		}catch(SQLException e) {
+			System.out.println("失敗");
+		}
+		return null;
+	}
+	public ResultSet memberselect() {
+		try {			
+			open();
+			String sql = "SELECT userId, name, email FROM users WHERE NOT EXISTS(SELECT * FROM joins WHERE joins.userId=users.userId);";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			ResultSet rs = pStmt.executeQuery(sql);
+			close();
+			return rs;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("失敗");
+		}
+		return null;
 	}
 }
