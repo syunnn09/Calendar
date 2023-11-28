651cd9932ef6eb1e7504c9741cd631d2cb9ef017
rerere <20214105@kcska.onmicrosoft.com>
2023-11-28 15:03:57 +0900

    パスワード変更

---
+++ src/main/java/servlet/account/ChangeServlet.java
@@ -0,0 +1,52 @@
+package servlet.account;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.AccountDao;
+
+/**
+ * Servlet implementation class ChangeServlet
+ */
+@WebServlet("/ChangeServlet")
+@MultipartConfig(maxFileSize=1048576)
+public class ChangeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/account/change.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int userId = (int) session.getAttribute("userId");
+		request.setCharacterEncoding("UTF-8");
+		String password = request.getParameter("newPassword");
+		
+		AccountDao ad = new AccountDao();
+		boolean isSuccess = ad.changePassword(userId, password);
+		
+		if (!isSuccess) {
+			request.setAttribute("text", "パスワード変更に失敗しました。");
+			doGet(request, response);
+			return;
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/schedule/top.jsp");
+		dispatcher.forward(request, response);
+	}
+}
